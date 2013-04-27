95ee3fc6ff6d4c8fff65aaf73906e3bc15ec0295
reednj <reednj@gmail.com>
2013-04-27 14:56:25 +0200

    update deploy script to auto generate prod scripts

--- config/pr-deploy.sh
+++ config/pr-deploy.sh
@@ -6,4 +6,5 @@ cp  ~/code/redditstream.git/.htaccess ~/reddit-stream.com/
 cp ~/code/config_backup/redditstream/esql.dbpass.php ~/reddit-stream.com/config/
 
 # now we need to create the cat'ed js and css files
-
+cat ~/reddit-stream.com/content/js/mootools-1.4.0.1-more.js ~/reddit-stream.com/content/js/rs-lib.js ~/reddit-stream.com/content/js/comments.js > ~/reddit-stream.com/content/js/thread.js
+cat ~/reddit-stream.com/content/css/base.css ~/reddit-stream.com/content/css/mobile.css > ~/reddit-stream.com/content/css/thread.css
